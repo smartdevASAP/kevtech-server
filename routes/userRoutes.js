8ed5ca94789c73a1d7b247a5e507653119a2ad8f
kelvin <kelvinnjuguna357@gmail.com>
2025-07-14 15:07:00 +0300

    Add ping route to router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,23 @@
+// const express = require("express");
+// const router = express.Router();
+// const userControllers = require("../controllers/userControllers.js");
+
+// router.post("/login", userControllers.login);
+// router.post("/register", userControllers.register);
+// router.post("/logout", userControllers.logout);
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/userControllers.js");
 
+// HEALTH CHECK inside /api/home
+router.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
 router.post("/login", userControllers.login);
 router.post("/register", userControllers.register);
 router.post("/logout", userControllers.logout);
+
 module.exports = router;
